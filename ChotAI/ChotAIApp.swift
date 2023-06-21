53c031c7f963a24a41ba9e5738a31e44580dc6bb
anthonygavriel <anthonygavriel@gmail.com>
2023-06-21 22:18:58 +0300

    Initial Commit

---
+++ ChotAI/ChotAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChotAIApp.swift
+//  ChotAI
+//
+//  Created by Anthony Gavriel on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChotAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
